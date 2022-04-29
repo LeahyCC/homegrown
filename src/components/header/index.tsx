409c348ac2e029887cecfbc1e42c851415a12762
Colin Leahy <ccleahy00@hotmail.com>
2022-04-28 19:50:34 -0600

    config updates

---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+const AppHeader = () => {
+  const x = 'hello world!'
+
+  return (
+    <div className="page-header__wrapper">
+      <h1>Super Awesome App! {x}</h1>
+    </div>
+  )
+}
+
+export default AppHeader
