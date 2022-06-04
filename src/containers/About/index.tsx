33e2ed74c057f84c6aad0720b21dd863b9b9fc8d
Colin Leahy <ccleahy00@hotmail.com>
2022-06-03 21:52:59 -0600

    nick cage in a cage

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState, useRef } from 'react'
 import InnerPageContainer from '@/components/InnerPageWrapper'
+import nickImg from './nick.png'
 
 const About = () => {
+  const [pointer, setPointer] = useState({ x: 0, y: 0 })
+  const mouseWrapRef = useRef<any>(null)
+
+  useEffect(() => {
+    const handler = (e: any) => {
+      setPointer({ x: e.clientX, y: e.clientY })
+    }
+    mouseWrapRef.current?.addEventListener('pointermove', handler)
+    return () => {
+      mouseWrapRef.current?.removeEventListener('pointermove', handler)
+    }
+  }, [])
+
   return (
     <InnerPageContainer>
-      <div className="page-header__wrapper">About page!</div>
+      X: {pointer.x}
+      <br />
+      Y: {pointer.y}
+      <br />
+      <br />
+      <img
+        src={nickImg}
+        alt="nick"
+        style={{
+          position: 'absolute',
+          left: pointer.x,
+          top: pointer.y,
+          height: '150px',
+          width: '150px',
+        }}
+      />
+      <div
+        style={{
+          borderRadius: '3px',
+          border: '3px solid lightblue',
+          height: '80vh',
+        }}
+        ref={mouseWrapRef}
+      />
     </InnerPageContainer>
   )
 }
